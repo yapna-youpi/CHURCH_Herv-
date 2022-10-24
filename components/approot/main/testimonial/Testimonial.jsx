5fa0fc5a6feedc644d6c1c3e54bd68570b5d65ab
yapna-youpi <yapnayoupi@gmail.com>
2022-10-24 19:03:49 +0100

    suite du testimonial

--- components/approot/main/testimonial/Testimonial.jsx
+++ components/approot/main/testimonial/Testimonial.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
+import Image from 'next/image'
+import imag from '../../../../public/big.jpg'
 
 function Testimonial() {
   return (
     <div className='w-full flex  h-screen text-white  bg-[#100C0D] '>
-        <div className="testimonial-img w-8/12 border ">
-
+        <div className="testimonial-img w-6/12  flex justify-center items-center ">
+          <Image src={imag} placeholder='blur'  width="700px" height="500px"  />
         </div>
-        <div className="testimonial-text w-6/12 border border-red-500 z-0 bg-transparent">
-            <h4 className='mt-20 mb-2 font-semibold text-lg'>TESTIMONIALS</h4>
+        <div className="testimonial-text w-6/12 z-0 bg-transparent">
+            <h4 className='mt-28 mb-2 font-semibold text-lg'>TESTIMONIALS</h4>
             <h2 className='mb-10 text-5xl font-bold'>Feedback About Us</h2>
 
             <p className='leading-10 w-10/12'>Lorem ipsum dolor sit amet consectetur, adipisicing elit. 
