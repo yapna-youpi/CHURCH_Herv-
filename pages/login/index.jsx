4ad4f49f67fa76ae0f1de130efed997bc92a93ba
yapna-youpi <yapnayoupi@gmail.com>
2022-12-29 23:49:04 +0100

    ajout de la partie du formulaire et interaction au click

--- pages/login/index.jsx
+++ pages/login/index.jsx
@@ -1,22 +1,58 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Header from '../../components/header/Header'
 import Footer from '../../components/footer/Footer'
 
 function index() {
+
+  useEffect(()=>{
+
+  },[])
+
+  
+  const handleSignin = function () {
+    const formBx = document.querySelector('.formBx');
+    const signin = document.querySelector('.signin')
+    formBx.classList.remove('activer');
+    formBx.classList.add('desactiver');
+    signin.classList.toggle('activer');
+    console.log('la classe retirer')
+  }
+  const handleSignup = function () {
+    const formBx = document.querySelector('.formBx');
+    const signin = document.querySelector('.signin')
+    formBx.classList.add('activer');
+    formBx.classList.remove('desactiver');
+    signin.classList.toggle('activer');
+    console.log('la classe activer')
+  }
   return (
     <div>
       <Header/>
-        <div className="flex justify-center items-center h-screen transition-all duration-500 bg-orange-300 ">
-            <div className="container relative w-6/12 h-96 flex ">
+        <div className="signin flex justify-center items-center h-screen transition-all duration-500 bg-orange-300 ">
+            <div className="container  relative w-6/12 h-96 flex">
               <div className="blueBg flex justify-center items-center absolute top-0 left-0 bg-orag">
                 <div className="box signin ">
                   <h2 className=''>Already Have An Account ?</h2>
-                  <button className='signinBtn'>Sign In</button>
+                  <button onClick={handleSignin} className='signinBtn'>Sign In</button>
                 </div>
                 <div className="box signup ">
                   <h2>Don't Have An Account ?</h2>
-                  <button className='signupBtn'>Sign Up</button>
+                  <button onClick={handleSignup} className='signupBtn'>Sign Up</button>
                 </div>
+              </div>
+              <div className="formBx absolute top-0 lef-0 w-6/12 bg-white flex justify-center items-center"> 
+                <form action="">
+                  <div className="formGroup">
+                    <p className='m-0 p-0 font-bold'>Username</p>
+                    <input type="text" name="name" placeholder="Online" />
+                  </div>
+                  <div className="formGroup">
+                    <p className='m-0 p-0 font-bold'>Password</p>
+                    <input type="password" name="password" placeholder="......" />
+                    <input type="submit" name='submit' value="Singn In" />
+                  </div>
+                  <a href='#'>Forget Password</a>
+                </form>
               </div>
             </div>
             {/* <div className='bubble '>
